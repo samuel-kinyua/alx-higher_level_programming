1a0254d47be8907a7ceb33f04f3ae991b34fe426
samuel-kinyua <samuelkinyua722@gmail.com>
2022-12-07 12:40:53 -0800

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name_ == _"__main__":
+    import hidden_4
+
+    names_to_print = dir(hidden_4)
+    for names_to_print in name:
+        if name_to_print[:2] != "__":
+            print(names_to_print)

b1d506cd406adc2f522d01ab1b862fd4247cab8c
samuel-kinyua <samuelkinyua722@gmail.com>
2022-12-06 23:49:25 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3i
-if __name__ == "__main__"
+if __name__ == "__main__":
     from sys import argv
-    num_of_args = len(argv)
+    num_of_args = len(argv):
         if num_of_args == 1:
             print("0 arguments.")
         elif num_of_args == 2:
             print("{} argument:".format(num_of_args - 1))
         else:
-	    print("{} arguments:".format(num_of_args - 1))
-        for index, var in enumerate(argv)
-	    if index == 0
-	        continue
+            print("{} arguments:".format(num_of_args - 1))
+        for index, var in enumerate(argv):
+            if index == 0:
+                continue
             print("{}: {}".format(index, var))
-
